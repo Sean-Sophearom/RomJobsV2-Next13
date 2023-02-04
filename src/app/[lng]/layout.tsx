d3317048057c7f75e9cd77e97b28d283f03178e7
SeanSophearom <sean.sophearom77@gmail.com>
2023-02-04 10:52:00 +0700

    Move font style to css and add khmer font family

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -2,7 +2,6 @@ import { LangProps } from "@/app/i18n";
 import { dir } from "i18next";
 
 import "@/styles/globals.css";
-import { Inter } from "@next/font/google";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { Navbar } from "@/components/ServerComp";
@@ -11,15 +10,13 @@ interface LayoutProps extends LangProps {
   children: React.ReactNode;
 }
 
-const inter = Inter({ subsets: ["latin"] });
-
 export default async function RootLayout({ children, params: { lng } }: LayoutProps) {
   const session = await getServerSession(authOptions);
   return (
     <html lang={lng} dir={dir(lng)}>
       <head />
-      <body className={inter.className}>
-        <Navbar session={session} />
+      <body>
+        <Navbar session={session} lng={lng} />
         {children}
       </body>
     </html>
